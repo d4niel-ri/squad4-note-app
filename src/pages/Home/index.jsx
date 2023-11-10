350d9e9d653bc756377d15c30591f204d71c9985
andikahiro21 <andika.pratama@phincon.com>
2023-11-10 14:55:32 +0700

    feat(FSB3-21): Add logout dropdown on navbar

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,22 +3,15 @@ import { useDispatch } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
 
 import { ping } from '@containers/App/actions';
-import { logoutRequest } from '@containers/Client/actions';
 import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleLogout = () => {
-    localStorage.removeItem('user');
-    dispatch(logoutRequest());
-    navigate('/login');
-  };
   return (
     <div>
       <FormattedMessage id="app_greeting" />
-      <button onClick={handleLogout}>LOGOUT</button>
     </div>
   );
 };
