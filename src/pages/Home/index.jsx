d173f1a94af7cb61ee7b5fe5523e957d8b8f5500
Muiz09 <muhammad.muiz@phincon.com>
2023-11-10 13:26:24 +0700

    feat(FSB3-21): PAGE HOME AND CREATE NOTE

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { connect, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { createStructuredSelector } from 'reselect';
 import { useNavigate } from 'react-router-dom';
-import DeleteIcon from '@mui/icons-material/Delete';
+import NoteCard from '@components/NoteCard/NoteCard';
 import { selectNotes } from './selectors';
-import { getAllNotes, deleteNote } from './actions';
+import { getAllNotes } from './actions';
 import classes from './style.module.scss';
 
 const Home = ({ notes }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleDelete = (id) => {
-    dispatch(deleteNote(id));
-  };
   useEffect(() => {
     dispatch(getAllNotes());
   }, []);
@@ -28,13 +25,7 @@ const Home = ({ notes }) => {
       </div>
       <div className={classes.conGrid}>
         {notes?.map((el) => (
-          <div key={el.id} className={classes.conCard}>
-            <h6>{el.title}</h6>
-            <div className={classes.description} dangerouslySetInnerHTML={{ __html: el?.description }} />
-            <div className={classes.deleteIconContainer}>
-              <DeleteIcon onClick={() => handleDelete(el.id)} />
-            </div>
-          </div>
+          <NoteCard key={el.id} id={el.id} title={el.title} description={el.description} />
         ))}
       </div>
     </div>
